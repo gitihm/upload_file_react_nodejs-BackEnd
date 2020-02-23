3ac2e44f89f83010e7cc9826d0d80441fa8850d9
air <s5935512030@phuket.psu.ac.th>
2020-02-23 13:56:09 +0700

    add method get img

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require("cors");
 const multer = require("multer");
 const port = 8000
+const server = `http://localhost:${port}`
+
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req,file,cb)=>{
-      cb(null,Date.now()+'_'+file.originalname.split(' ').join())
+      cb(null,Date.now()+'_'+file.originalname.split(' ').join().replace(',','_'))
   }
 });
 app.use(cors())
@@ -21,15 +23,15 @@ app.post("/upload",(req,res)=>{
                 res.json({message:'error: no file selected'})
             }
             else{
-                res.json({message:'file uploaded', url : `http://localhost:${port}/uploads/${req.file.filename}`})
+                res.json({message:'file uploaded', url : `${server}/upload/${req.file.filename}`})
             }
         }
     })
     
 });
-// app.get("/upload",uploader.single('file'),(req,res)=>{
-//     res.json({message:uploader.single('file').name})
-// });
+app.get("/upload/:path",(req,res)=>{
+    return res.sendFile(req.params.path, { root: 'uploads/' });
+});
 
 app.listen(port,(err)=>{
     if (err) throw err
