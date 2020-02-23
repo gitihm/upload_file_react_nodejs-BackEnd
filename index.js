2104ebff6cdf7c4ddd23026c9f16869b8f67ea88
air <s5935512030@phuket.psu.ac.th>
2020-02-23 14:06:05 +0700

    add checkfiletype

--- index.js
+++ index.js
@@ -2,39 +2,64 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const multer = require("multer");
-const port = 8000
-const server = `http://localhost:${port}`
+const path = require("path");
+const port = 8000;
+const server = `http://localhost:${port}`;
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
-  filename: (req,file,cb)=>{
-      cb(null,Date.now()+'_'+file.originalname.split(' ').join().replace(',','_'))
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      Date.now() +
+        "_" +
+        file.originalname
+          .split(" ")
+          .join()
+          .replace(",", "_")
+    );
   }
 });
-app.use(cors())
-let uploader = multer({storage}).single('file')
-app.post("/upload",(req,res)=>{
-    uploader(req,res,(err)=>{
-        if(err) res.json({message:'error'})
-        else{
-            if(req.file == undefined){
-                res.json({message:'error: no file selected'})
-            }
-            else{
-                res.json({message:'file uploaded', url : `${server}/upload/${req.file.filename}`})
-            }
-        }
-    })
-    
+checkFileType = (file, cb) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("images only");
+  }
+};
+app.use(cors());
+let uploader = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  }
+}).single("file");
+app.post("/upload", (req, res) => {
+  uploader(req, res, err => {
+    if (err) res.json({ message: "error", details: err });
+    else {
+      if (req.file == undefined) {
+        res.json({ message: "error", details: "no file selected" });
+      } else {
+        res.json({
+          message: "file uploaded",
+          url: `${server}/upload/${req.file.filename}`
+        });
+      }
+    }
+  });
 });
-app.get("/upload/:path",(req,res)=>{
-    return res.sendFile(req.params.path, { root: 'uploads/' });
+app.get("/upload/:path", (req, res) => {
+  return res.sendFile(req.params.path, { root: "uploads/" });
 });
 
-app.listen(port,(err)=>{
-    if (err) throw err
-      console.log(`> Ready on http://localhost:${port}`)
-    
-})
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
+});
